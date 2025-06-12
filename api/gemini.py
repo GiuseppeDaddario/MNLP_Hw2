9b123f15da3b8410074cdf242d264f303ae95193
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-06-12 16:46:17 +0200

    (add) gemini info on the limitations

--- api/gemini.py
+++ api/gemini.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 
 ## This function sends a message to the Gemini AI model and returns the response
+## LIMITATIONS: we have 500 RPD (Requests Per Day) for the free tier
 def ask_gemini(message):
     genai.configure(api_key="AIzaSyAvfJXiEgC25NBZh2qkWT6LkbSgGDSPgrc")
     model = genai.GenerativeModel(model_name="gemini-2.0-flash")
