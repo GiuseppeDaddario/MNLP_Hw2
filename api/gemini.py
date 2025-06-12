64b4684f0f97caa7447c6f985ed0b05ea4551c5e
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-06-12 16:42:58 +0200

    adding gemini api

---
+++ api/gemini.py
@@ -0,0 +1,9 @@
+import google.generativeai as genai
+
+## This function sends a message to the Gemini AI model and returns the response
+def ask_gemini(message):
+    genai.configure(api_key="AIzaSyAvfJXiEgC25NBZh2qkWT6LkbSgGDSPgrc")
+    model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+    response = model.generate_content(message)
+
+    return response.text
