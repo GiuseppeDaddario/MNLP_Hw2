faf94a24f59313becbc45f1567da0db61049c676
lorebenucci <lore.benucci@gmail.com>
2025-06-13 14:51:40 +0200

    hugging login lorenzo

---
+++ src/login.py
@@ -0,0 +1,13 @@
+## LORE
+
+# huggingface-cli login in order to use the model
+
+
+from huggingface_hub import login
+
+# Inserisci qui il tuo token personale
+hf_token = "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"
+
+login(token=hf_token)
+
+## LORE
