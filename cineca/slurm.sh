8cd7a204bf998077d5766cbf4f13a40a7fb9c04e
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-06-14 10:10:25 +0200

    (fix) slurm.sh

--- cineca/slurm.sh
+++ cineca/slurm.sh
@@ -4,9 +4,9 @@
 #SBATCH --job-name=multi_gpu_job              # Descriptive job name
 #SBATCH --time=04:00:00                       # Maximum wall time (hh:mm:ss)
 #SBATCH --nodes=1                             # Number of nodes to use
-#SBATCH --ntasks-per-node=4                   # Number of MPI tasks per node (e.g., 1 per GPU)
-#SBATCH --cpus-per-task=10                    # Number of CPU cores per task (adjust as needed)
-#SBATCH --gres=gpu:4                          # Number of GPUs per node (adjust to match hardware)
+#SBATCH --ntasks-per-node=1                   # Number of MPI tasks per node (e.g., 1 per GPU)
+#SBATCH --cpus-per-task=1                     # Number of CPU cores per task (adjust as needed)
+#SBATCH --gres=gpu:1                          # Number of GPUs per node (adjust to match hardware)
 #SBATCH --partition=lrd_all_serial            # GPU-enabled partition
 #SBATCH --qos=normal                          # Quality of Service
 #SBATCH --output=multiGPUJob.out              # File for standard output
