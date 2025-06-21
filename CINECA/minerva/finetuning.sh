a3b28966f13a93793d7ae072d1e80975cfe69eb2
Giuseppe D'Addario <gdaddari@login02.leonardo.local>
2025-06-21 18:17:32 +0200

    logs

---
+++ CINECA/minerva/finetuning.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=llimapostocr
+#SBATCH --time=04:00:00                       # Maximum wall time (hh:mm:ss)
+#SBATCH --nodes=1                             # Number of nodes to use
+#SBATCH --ntasks-per-node=1                   # Number of MPI tasks per node (e.g., 1 per GPU)
+#SBATCH --cpus-per-task=8                     # Number of CPU cores per task (adjust as needed)
+#SBATCH --gres=gpu:2                          # Number of GPUs per node (adjust to match hardware)
+#SBATCH --partition=boost_usr_prod            # GPU-enabled partition
+#SBATCH --qos=normal                          # Quality of Service
+#SBATCH --output=CINECA/logs/minerva_finetuning_llima_postocr.out
+#SBATCH --error=CINECA/logs/minerva_finetuning_llima_postocr.err
+#SBATCH --account=try25_navigli               # Project account number
+
+
+
+# === Carica i moduli necessari ===
+module load cuda/12.1
+module load python/3.10
+
+# === Attiva il tuo ambiente virtuale o Conda ===
+source ~/mnlp/bin/activate  # Sostituisci col path corretto
+
+# === Vai nella directory dove si trova lo script ===
+cd $SLURM_SUBMIT_DIR
+
+# === Avvia lo script di inferenza ===
+python src/models/minerva/finetuning.py
